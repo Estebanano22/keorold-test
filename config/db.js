ae783d53f62662f1d227ba913764f4ee467c7acc
Estebanano22 <35642566+Estebanano22@users.noreply.github.com>
2021-07-04 02:07:23 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 const db = new Sequelize('fullentretenimiento_2021_ok', 'fullesteban2021', 'NewFullentretenimiento123*', {
-    host: 'full2021ok.c7npajsfxzst.us-east-2.rds.amazonaws.com',
+    host: 'aa16bhwczgzgm1p.c7npajsfxzst.us-east-2.rds.amazonaws.com',
     dialect: 'mysql',
     port: 3306,
     dialectOptions: {
