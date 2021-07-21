15e0756a3001d87eb5130afcfc0c6fb819475fb9
Estebanano22 <35642566+Estebanano22@users.noreply.github.com>
2021-07-21 03:23:38 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -7,10 +7,11 @@ dotenv.config({
 });
 
 const db = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
+    host: '/cloudsql/'+process.env.DB_HOST,
     dialect: 'mysql',
     port: 3306,
     dialectOptions: {
+        socketPath: '/cloudsql/'+process.env.SOCKETPATH),
         charset: 'utf8_general_ci'
     },
     define: {
