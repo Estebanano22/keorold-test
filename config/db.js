5172670e760fdc33f38b7ca3ce3c80700c422b5f
Esteban Baquero <esteban@digitek.com.co>
2021-08-30 13:51:04 -0500

    change conection

--- config/db.js
+++ config/db.js
@@ -10,9 +10,13 @@ dotenv.config({
 const db = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
     dialect: 'mysql',
-    port: 3306,
-    dialectOptions: {
-        charset: 'utf8_general_ci'
+    port: process.env.DB_PORT,
+    dialectOptions: { 
+        ssl: { 
+            require: true,
+            rejectUnauthorized: false
+        },
+        charset: 'utf8_general_ci',
     },
     define: {
         timestamps: false
