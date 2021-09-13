24a317b3a6245ae6675b9ecbb3f5cd85369b55b2
Dilan Ariza - Digiworld <dilan.dev@digiworld.com.co>
2021-09-13 11:43:13 -0500

    delete console log

--- controllers/reportesController.js
+++ controllers/reportesController.js
@@ -2074,7 +2074,6 @@ exports.reporteGanancias = async (req, res) => {
 
 exports.reporteConsignacionesDistribuidor = async (req, res) => {
 
-    console.log(req.body);
     const fecha = req.body.fecha.split(' - ');
     const idUsuarioReporte = req.body.user;
     const fecha1 = moment(new Date(fecha[0])).format("YYYY-MM-DD");
@@ -2091,7 +2090,6 @@ exports.reporteConsignacionesDistribuidor = async (req, res) => {
             ],
             order: [['fecha', 'DESC']]
         });
-        console.log(ganancias);
     } else {
         var ganancias = await Ganancias.findAll({
             where: {
