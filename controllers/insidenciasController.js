9ce3fb5631375a97edd2cd1c7e3af64cfeffa962
DilanPortatil <dilan.dev@digiworld.com.co>
2021-11-29 11:44:49 -0500

    fix error to asign incidencias

--- controllers/insidenciasController.js
+++ controllers/insidenciasController.js
@@ -97,7 +97,7 @@ exports.crearInsidencia = async (req, res) => {
 
     const superdistribuidor = await Usuarios.findOne({ where: { enlace_afiliado: req.user.super_patrocinador } });
 
-    if (!req.location || req.file === undefined || req.file === null) {
+    if (req.file === undefined || req.file === null) {
         var nombreArchivo = null;
     } else {
         if (req.file.location === 'undefined' || req.file.location === '' || req.file.location === null) {
