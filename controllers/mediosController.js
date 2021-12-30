9f66687733afb8969fd97259f35b8d1c7b6508c4
Dilan Ariza - Digiworld <dilan.dev@digiworld.com.co>
2021-12-30 15:22:03 -0500

    add hotfixs branchs

--- controllers/mediosController.js
+++ controllers/mediosController.js
@@ -1,25 +1,26 @@
 const Usuarios = require('../models/UsuariosModelo');
 const Medios = require('../models/mediosModelo');
 const { Op } = require("sequelize");
-const {body, validationResult} = require('express-validator');
+const { body, validationResult } = require('express-validator');
 const multer = require('multer');
 const shortid = require('shortid');
 const { v4: uuid_v4 } = require('uuid');
 const fs = require('fs');
-const {s3, bucket} = require('../config/awsS3');
+const { s3, bucket } = require('../config/awsS3');
 const multerS3 = require('multer-s3');
+console.log('hotfixes');
 
-exports.mediosConsignacion = async (req,res) => {
+exports.mediosConsignacion = async (req, res) => {
 
-    const usuario = await Usuarios.findOne({ where: { id_usuario: req.user.id_usuario }});
-    const medios = await Medios.findAll({ 
+    const usuario = await Usuarios.findOne({ where: { id_usuario: req.user.id_usuario } });
+    const medios = await Medios.findAll({
         where: {
-            [Op.and]: [{idSuperdistribuidor: req.user.id_usuario}]
+            [Op.and]: [{ idSuperdistribuidor: req.user.id_usuario }]
         }
     });
 
     res.render('dashboard/mediosConsignacion', {
-        nombrePagina : 'Medios de consignación',
+        nombrePagina: 'Medios de consignación',
         titulo: 'Medios de consignación',
         breadcrumb: 'Medios de consignación',
         classActive: req.path.split('/')[2],
@@ -49,8 +50,8 @@ const upload = multer(configuracionMulter).any('imagen', 'recurso');
 
 exports.uploadRecursos = async (req, res, next) => {
 
-    upload(req, res, function(error) {
-        if(error){
+    upload(req, res, function (error) {
+        if (error) {
             res.json({ titulo: '¡Lo Sentimos!', resp: 'error', descripcion: 'Hubo un error con el archivo que desea subir en logo' });
             return;
         } else {
@@ -61,7 +62,7 @@ exports.uploadRecursos = async (req, res, next) => {
 
 exports.subirMedio = async (req, res) => {
     console.log(req.body.imagen);
-    if(req.body.imagen === 'undefined') {
+    if (req.body.imagen === 'undefined') {
         res.json({ titulo: '¡Lo Sentimos!', resp: 'error', descripcion: 'Por favor suba todos los recursos ya que son obligatorios.' });
         return;
     }
@@ -70,19 +71,19 @@ exports.subirMedio = async (req, res) => {
     const descripcionCorta = req.body.descripcionCortaMedio.trim();
     const descripcion = req.body.descripcionMedio.trim();
     const imagen = req.files[0].location;
-    if(req.body.recurso !== 'undefined') {
+    if (req.body.recurso !== 'undefined') {
         var recurso = req.files[1].location;
     } else {
         var recurso = null;
     }
 
-    if(titulo === '' || descripcionCorta === '' || descripcion === '') {
+    if (titulo === '' || descripcionCorta === '' || descripcion === '') {
         res.json({ titulo: '¡Lo Sentimos!', resp: 'error', descripcion: 'Debe llenar todos los campos.' });
         return;
     }
 
     try {
-        
+
         await Medios.create({
             idMedio: uuid_v4(),
             idSuperdistribuidor: req.user.id_usuario,
@@ -93,14 +94,14 @@ exports.subirMedio = async (req, res) => {
             imagen: imagen,
             recurso: recurso
         });
-    
+
         res.json({ titulo: '¡Que bien!', resp: 'success', descripcion: 'Medio de consignación creado con éxito.' });
         return;
 
     } catch (error) {
         console.log(error);
         res.json({ titulo: '¡Lo sentimos!', resp: 'error', descripcion: error.message });
-        return; 
+        return;
     }
 
 }
@@ -109,7 +110,7 @@ exports.infoMedio = async (req, res) => {
 
     const id = req.body.id;
 
-    const medios = await Medios.findOne({ where: { idMedio: id }});
+    const medios = await Medios.findOne({ where: { idMedio: id } });
 
     res.json({ medio: medios });
     return;
@@ -123,16 +124,16 @@ exports.editarMedio = async (req, res) => {
     const descripcionCorta = req.body.descripcionCorta;
     const descripcion = req.body.descripcion;
 
-    if(id === '' || titulo === '' || descripcionCorta === '' || descripcion === '') {
+    if (id === '' || titulo === '' || descripcionCorta === '' || descripcion === '') {
 
         res.json({ titulo: '¡Lo Sentimos!', resp: 'error', descripcion: 'Debe llenar todos los campos.' });
         return;
 
     }
 
-    const medio = await Medios.findOne({ 
+    const medio = await Medios.findOne({
         where: {
-            [Op.and]: [{idMedio: id}]
+            [Op.and]: [{ idMedio: id }]
         }
     });
 
@@ -151,13 +152,13 @@ exports.bloquearMedio = async (req, res) => {
 
     const id = req.body.id;
 
-    const medio = await Medios.findOne({ 
+    const medio = await Medios.findOne({
         where: {
-            [Op.and]: [{idMedio: id}]
+            [Op.and]: [{ idMedio: id }]
         }
     });
 
-    if(medio.estado === 1) {
+    if (medio.estado === 1) {
         var estado = 0;
     } else {
         var estado = 1;
@@ -176,13 +177,13 @@ exports.eliminarMedio = async (req, res) => {
 
     const id = req.body.id;
 
-    const medio = await Medios.findOne({ 
+    const medio = await Medios.findOne({
         where: {
-            [Op.and]: [{idMedio: id}]
+            [Op.and]: [{ idMedio: id }]
         }
     });
 
-    if(!medio) {
+    if (!medio) {
         res.json({ titulo: '¡Lo Sentimos!', resp: 'error', descripcion: 'No es posible eliminar el medio de consignación.' });
         return;
     }
@@ -196,7 +197,7 @@ exports.eliminarMedio = async (req, res) => {
         console.log('recurso removido')
     })
 
-    await medio.destroy({ where: {idMedio: id}});
+    await medio.destroy({ where: { idMedio: id } });
 
     res.json({ titulo: '¡Que bien!', resp: 'success', descripcion: 'El medio de consignación se elimino con éxito.' });
     return;
