c94637cb5c290c57f408a66f2f8dfe7748e91505
Juan Baquero <baqueromorenoj@gmail.com>
2022-09-07 15:44:14 -0500

    STABLE . 10 data admin juegos

--- controllers/cuentasController.js
+++ controllers/cuentasController.js
@@ -1554,7 +1554,7 @@ exports.adminCuentasJuegos = async (req, res) => {
             ['estado', 'asc'],
             ['fechaSubida', 'desc'],
         ],
-        limit: 300,
+        limit: 10,
     });
     const cuentasJuegos = await Cuentas.count({
         where: {
