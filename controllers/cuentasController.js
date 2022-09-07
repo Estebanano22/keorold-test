c05e29d68dec701a7ca82162955879602a1f75e4
Juan Baquero <baqueromorenoj@gmail.com>
2022-09-07 15:47:42 -0500

    ÂSTABLE - add data to 400 juegos cuentas

--- controllers/cuentasController.js
+++ controllers/cuentasController.js
@@ -1554,7 +1554,7 @@ exports.adminCuentasJuegos = async (req, res) => {
             ['estado', 'asc'],
             ['fechaSubida', 'desc'],
         ],
-        limit: 400,
+        limit: 350,
     });
     const cuentasJuegos = await Cuentas.count({
         where: {
